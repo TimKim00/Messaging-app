4ddd0019d52ca3ecac3960427437c562ca69a61a
Timothy <ystimokk@gmail.com>
2024-08-23 13:40:13 -0700

    cookie retry

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const URL =
   import.meta.env.MODE === "production"
-    ? "https://messaging-app-sigma-seven.vercel.app"
+    ? "https://messaging-app-drj8.onrender.com"
     : "http://localhost:3000";
 
 export const socket = io(URL, {
