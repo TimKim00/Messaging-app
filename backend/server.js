ea19749ce16cdd6b2609c4438d9a90fa4d53c32c
Timothy <ystimokk@gmail.com>
2024-09-06 23:38:07 -0700

    test with different cookie setting

--- backend/server.js
+++ backend/server.js
@@ -57,6 +57,7 @@ app.use(
       secure: process.env.NODE_ENV === "production", // Only set cookies over HTTPS in production
       httpOnly: true, // Prevents JavaScript access to the cookie
       sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // Adjusts for cross-site requests
+      domain: process.env.NODE_ENV === "production" && ".onrender.com",
     },
   })
 );
